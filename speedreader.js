8ddbc6774dba82c749e5add60ce7fd776ca1248c
Marshmellowon <leo503801@gmail.com>
2020-04-09 18:36:56 +0900

    js start

--- speedreader.js
+++ speedreader.js
@@ -1,7 +1,14 @@
 "use strict";
 
 (function () {
-  function scriptload() {}
+  function scriptload() {
+    let sp = document.getElementById("start");
+    sp.addEventListener("click", splitword);
+  }
 
+  function splitword() {
+    let word = document.getElementById("inputtxt").value;
+    document.getElementById("showtxt").innerHTML = word;
+  }
   window.onload = scriptload;
 })();
